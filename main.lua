bcce7ac9555a8e19153fed63592dfb378fddf6f5
e9188608-svg <e9188608@gmail.com>
2026-04-26 05:31:02 +0300

    Refactor remote handling and UI components
    Updated remote variable and UI library. Added new toggles and sliders for pitch and jitter settings.

--- main.lua
+++ main.lua
@@ -1,239 +1,146 @@
-local remote = game:GetService("ReplicatedStorage"):WaitForChild("aahelp")
+local remote = game:GetService("ReplicatedStorage"):WaitForChild("aahelp1")
+local remote1 = game:GetService("ReplicatedStorage"):WaitForChild("aahelp")
 
 local myArgs = {
     "apply",
-    170, 
+    0, 
+    0   
+}
+
+local myArgs1 = {
+    "apply",
+    0, 
     0   
 }
 
 local jitterEnabled = false
+local jitterEnabled1 = false
 local jitterRandom = false
+local jiter_body = 0
+local rng = Random.new()
+local result = rng:NextInteger(-359, 359)
+local result1 = rng:NextInteger(-180, 180)
 local UserInputService = game:GetService("UserInputService");
+local root = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
 
---// Library
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/lates-lib/main/Main.lua"))()
-local Window = Library:CreateWindow({
-	Title = "???",
-	Theme = "Dark",
-	
-	Size = UDim2.fromOffset(570, 370),
-	Transparency = 0.2,
-	Blurring = true,
-	MinimizeKeybind = Enum.KeyCode.LeftAlt,
+local uiLoader = loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/dollarware/main/library.lua'))
+local ui = uiLoader({
+    rounding = false, 
+    theme = 'cherry',
+    smoothDragging = false
 })
 
-local Themes = {
-	Light = {
-		--// Frames:
-		Primary = Color3.fromRGB(232, 232, 232),
-		Secondary = Color3.fromRGB(255, 255, 255),
-		Component = Color3.fromRGB(245, 245, 245),
-		Interactables = Color3.fromRGB(235, 235, 235),
+ui.autoDisableToggles = true
 
-		--// Text:
-		Tab = Color3.fromRGB(50, 50, 50),
-		Title = Color3.fromRGB(0, 0, 0),
-		Description = Color3.fromRGB(100, 100, 100),
-
-		--// Outlines:
-		Shadow = Color3.fromRGB(255, 255, 255),
-		Outline = Color3.fromRGB(210, 210, 210),
-
-		--// Image:
-		Icon = Color3.fromRGB(100, 100, 100),
-	},
-	
-	Dark = {
-		--// Frames:
-		Primary = Color3.fromRGB(30, 30, 30),
-		Secondary = Color3.fromRGB(35, 35, 35),
-		Component = Color3.fromRGB(40, 40, 40),
-		Interactables = Color3.fromRGB(45, 45, 45),
-
-		--// Text:
-		Tab = Color3.fromRGB(200, 200, 200),
-		Title = Color3.fromRGB(240,240,240),
-		Description = Color3.fromRGB(200,200,200),
-
-		--// Outlines:
-		Shadow = Color3.fromRGB(0, 0, 0),
-		Outline = Color3.fromRGB(40, 40, 40),
-
-		--// Image:
-		Icon = Color3.fromRGB(220, 220, 220),
-	},
-	
-	Void = {
-		--// Frames:
-		Primary = Color3.fromRGB(15, 15, 15),
-		Secondary = Color3.fromRGB(20, 20, 20),
-		Component = Color3.fromRGB(25, 25, 25),
-		Interactables = Color3.fromRGB(30, 30, 30),
-
-		--// Text:
-		Tab = Color3.fromRGB(200, 200, 200),
-		Title = Color3.fromRGB(240,240,240),
-		Description = Color3.fromRGB(200,200,200),
-
-		--// Outlines:
-		Shadow = Color3.fromRGB(0, 0, 0),
-		Outline = Color3.fromRGB(40, 40, 40),
-
-		--// Image:
-		Icon = Color3.fromRGB(220, 220, 220),
-	},
-
-}
-
---// Set the default theme
-Window:SetTheme(Themes.Dark)
-
---// Sections
-Window:AddTabSection({
-	Name = "Main",
-	Order = 1,
+local window = ui.newWindow({
+    text = 'Dollarware', 
+    resize = true, 
+    size = Vector2.new(550, 376), 
+    position = nil 
 })
 
-Window:AddTabSection({
-	Name = "Settings",
-	Order = 2,
+local menu = window:addMenu({
+    text = 'aa'
 })
+do 
+    
+    local section = menu:addSection({
+        text = 'pitch',
+        side = 'auto',
+        showMinButton = false
+    })
+    do 
+	        local toggle = section:addToggle({
+            text = 'toggle pitch', 
+            state = false 
+        }, function(togglee)
+			jitterEnabled = togglee
+		end)
+        section:addSlider({
+            text = 'pitch',
+            min = -180,
+            max = 180,
+            step = 0.01,
+            val = 0
+        }, function(newValue) 
+            myArgs[2] = newValue
+        end):setTooltip('pitch')
 
---// Tab [MAIN]
+			local toggle = section:addToggle({
+            text = 'toggle rozdolbyayka', 
+            state = false 
+        }, function(togglee1)
+			jitterRandom = togglee1
+		end)
 
-local Main = Window:AddTab({
-	Title = "Components",
-	Section = "Main",
-	Icon = "rbxassetid://11963373994"
-})
+			local toggle = section:addToggle({
+            text = 'toggle jitter', 
+            state = false 
+        }, function(togglee11)
+			jitterEnabled1 = togglee11
+		end)
 
-Window:AddSection({ Name = "aa", Tab = Main }) 
+		    section:addSlider({
+            text = 'jetter power',
+            min = -359,
+            max = 359,
+            step = 0.01,
+            val = 0
+        }, function(jiter) 
+			jiter_body = jiter
+        end):setTooltip('jetter power')
 
-Window:AddSlider({
-	Title = "head tilt",
-	Description = "head tilt",
-	Tab = Main,
-	MaxValue = 180,
-	Callback = function(Amount) 
-		myArgs[2] = Amount
-	end,
-}) 
+    end
+    local menu1 = window:addMenu({
+    text = 'other'
+	})
+	do
+    	local section1 = menu1:addSection({
+        text = 'telegram',
+        side = 'auto',
+        showMinButton = true,
+    	})
+		do
+			section1:addButton({
+				text = 'TG NAHOI', 
+				style = 'small'
+			}):bindToEvent('onClick', function()
+				setclipboard("https://t.me/pidorasik6767gazanmango")
+				ui.notify({
+					title = 'LINK COPIED',
+					message = 'MANGO 67 67 67 TROLLFACE!',
+					duration = 3
+				})
+			end)
+		end
 
-Window:AddToggle({
-	Title = "Toggle head tilt",
-	Description = "Switching head tilt",
-	Tab = Main,
-	Callback = function(Boolean) 
-		jitterEnabled = Boolean
-	end,
-}) 
-
-Window:AddToggle({
-	Title = "Toggle head tilt R",
-	Description = "Switching head tilt R",
-	Tab = Main,
-	Callback = function(Boolean1) 
-		jitterRandom = Boolean1
-	end,
-})
-
--- Window:AddInput({
--- 	Title = "Input",
--- 	Description = "Typing",
--- 	Tab = Main,
--- 	Callback = function(Text) 
--- 		warn(Text);
--- 	end,
--- }) 
-
-
--- Window:AddDropdown({
--- 	Title = "Dropdown",
--- 	Description = "Selecting",
--- 	Tab = Main,
--- 	Options = {
--- 		["An Option"] = "hi",
--- 		["And another"] = "hi",
--- 		["Another"] = "hi",
--- 	},
--- 	Callback = function(Number) 
--- 		warn(Number);
--- 	end,
--- }) 
-
--- Window:AddKeybind({
--- 	Title = "Keybind",
--- 	Description = "Binding",
--- 	Tab = Main,
--- 	Callback = function(Key) 
--- 		warn("Key Set")
--- 	end,
--- }) 
-
---// Tab [SETTINGS]
-local Keybind = nil
-local Settings = Window:AddTab({
-	Title = "Settings",
-	Section = "Settings",
-	Icon = "rbxassetid://11293977610",
-})
-
-Window:AddKeybind({
-	Title = "Minimize Keybind",
-	Description = "Set the keybind for Minimizing",
-	Tab = Settings,
-	Callback = function(Key) 
-		Window:SetSetting("Keybind", Key)
-	end,
-}) 
-
-Window:AddDropdown({
-	Title = "Set Theme",
-	Description = "Set the theme of the library!",
-	Tab = Settings,
-	Options = {
-		["Light Mode"] = "Light",
-		["Dark Mode"] = "Dark",
-		["Extra Dark"] = "Void",
-	},
-	Callback = function(Theme) 
-		Window:SetTheme(Themes[Theme])
-	end,
-}) 
-
-Window:AddToggle({
-	Title = "UI Blur",
-	Description = "If enabled, must have your Roblox graphics set to 8+ for it to work",
-	Default = true,
-	Tab = Settings,
-	Callback = function(Boolean) 
-		Window:SetSetting("Blur", Boolean)
-	end,
-}) 
-
-
-Window:AddSlider({
-	Title = "UI Transparency",
-	Description = "Set the transparency of the UI",
-	Tab = Settings,
-	AllowDecimals = true,
-	MaxValue = 1,
-	Callback = function(Amount) 
-		Window:SetSetting("Transparency", Amount)
-	end,
-}) 
-
-Window:Notify({
-	Title = "Hello user!",
-	Description = "Press Left Alt to Minimize and Open the tab!", 
-	Duration = 10
-})
+		local section2 = menu1:addSection({
+        text = 'arcanum (crack)',
+        side = 'auto',
+        showMinButton = true,
+    	})
+		do
+			section2:addButton({
+				text = 'crack arcanum', 
+				style = 'small'
+			}):bindToEvent('onClick', function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHouses/f/main/ARCANUM.lua"))()
+			end)
+	end	end
+end
 
 task.spawn(function()
-    while task.wait(0.01) do
+    while task.wait(0) do
+		if jitterEnabled1 then
+			root.CFrame = root.CFrame * CFrame.Angles(0, math.rad(math.random(jiter_body, jiter_body)), 0)
+		end
         if jitterEnabled then
             if jitterRandom then
-                myArgs[2] = math.random(-180, 180)
+				result = rng:NextInteger(-359, 359)
+                myArgs[2] = result
+				remote:FireServer(unpack(myArgs))
+				result = rng:NextInteger(-359, 359)
+                myArgs[2] = result
             end
             remote:FireServer(unpack(myArgs))
         end
